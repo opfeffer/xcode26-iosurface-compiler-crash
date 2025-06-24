f20af0f795e223c1967b5609a48cc4e4cd04c808
Oli Pfeffer <pfeffero@amazon.com>
2025-06-24 19:25:55 -0400

    Initial Commit

---
+++ IOSurfaceCrash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSurfaceCrash
+//
+//  Created by Pfeffer, Oli on 6/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSurfaceCrash/IOSurfaceCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSurfaceCrashApp.swift
+//  IOSurfaceCrash
+//
+//  Created by Pfeffer, Oli on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSurfaceCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
